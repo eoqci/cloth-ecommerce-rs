74995b223f2d9b173f232446ad34b9922a492e40
eoqci <boyrovip1539@gmail.com>
2026-07-30 20:02:17 +0700

    feat: config change

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,4 @@
 use crate::error::ConfigError;
-
 use dotenvy::dotenv;
 use email_address::EmailAddress;
 use std::{env, str::FromStr};
@@ -41,35 +40,33 @@ impl Config {
 
     fn get_email_env(key: &str) -> Result<String, ConfigError> {
         let email = Self::get_env(key)?;
-
         EmailAddress::from_str(&email)
             .map_err(|_| ConfigError::InvalidEmailFormat(email.clone()))?;
-
         Ok(email)
+    }
+
+    fn get_number_env<T>(key: &str) -> Result<T, ConfigError>
+    where
+        T: FromStr,
+    {
+        let val = Self::get_env(key)?;
+        val.parse::<T>()
+            .map_err(|_| ConfigError::InvalidNumber(key.to_string(), val.clone()))
     }
 
     pub fn init() -> Result<Config, ConfigError> {
         dotenv().ok();
-
         Ok(Config {
             database_url: Self::get_env("DATABASE_URL")?,
-            redis_url: Self::get_env("REDIS_URl")?,
+            redis_url: Self::get_env("REDIS_URL")?,
             server_host: env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_string()),
-            server_port: Self::get_env("PORT")?
-                .parse::<u16>()
-                .map_err(|e| ConfigError::InvalidNumber("PORT".into(), e.to_string()))?,
+            server_port: Self::get_number_env::<u16>("PORT")?,
             domain_name: Self::get_env("DOMAIN_NAME")?,
             resend_api_key: Self::get_env("RESEND_API_KEY")?,
             from_email: Self::get_email_env("FROM_EMAIL")?,
             jwt_secret: Self::get_env("JWT_SECRET")?,
-            jwt_expired_in: Self::get_env("JWT_EXPIRED_IN")?
-                .parse::<i64>()
-                .map_err(|e| ConfigError::InvalidNumber("JWT_EXPIRED_IN".into(), e.to_string()))?,
-            refresh_token_expired_in: Self::get_env("REFRESH_TOKEN_EXPIRED_IN")?
-                .parse::<i32>()
-                .map_err(|e| {
-                    ConfigError::InvalidNumber("REFRESH_TOKEN_EXPIRED_IN".into(), e.to_string())
-                })?,
+            jwt_expired_in: Self::get_number_env::<i64>("JWT_EXPIRED_IN")?,
+            refresh_token_expired_in: Self::get_number_env::<i32>("REFRESH_TOKEN_EXPIRED_IN")?,
             //==============================| CLOUDFLARE |==============================
             cf_r2_access_key: Self::get_env("CF_R2_ACCESS_KEY")?,
             cf_r2_secret_key: Self::get_env("CF_R2_SECRET_KEY")?,
