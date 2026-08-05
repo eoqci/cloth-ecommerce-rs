965af2504dec6275ef7d5009f6ab8e2cede51335
eoqci <boyrovip1539@gmail.com>
2026-08-05 22:49:22 +0700

    test: temp hide module-state

--- src/modules/mod.rs
+++ src/modules/mod.rs
@@ -2,7 +2,7 @@ pub mod admin;
 pub mod auth;
 pub mod cart;
 pub mod category;
-pub mod module_state;
+// pub mod module_state;
 pub mod order;
 pub mod product;
 pub mod user;
--- src/modules/module_state.rs
+++ src/modules/module_state.rs
@@ -1,26 +1,26 @@
-use sqlx::PgPool;
+// use sqlx::PgPool;
 
-use crate::{
-    config::Config,
-    modules::{admin::state::AdminState, auth::state::AuthState, cart::state::CartState},
-};
+// use crate::{
+//     config::Config,
+//     modules::{admin::state::AdminState, auth::state::AuthState, cart::state::CartState},
+// };
 
-#[derive(Clone)]
-pub struct ModuleState {
-    pub admin_state: AdminState,
-    pub auth_state: AuthState,
-    // pub user_state: UserState,
-    pub cart_state: CartState,
-    pub category_state: CategoryState,
-    // pub product_state: ProductState,
-    // pub order_state: OrderState,
-}
+// #[derive(Clone)]
+// pub struct ModuleState {
+//     pub admin_state: AdminState,
+//     pub auth_state: AuthState,
+//     // pub user_state: UserState,
+//     pub cart_state: CartState,
+//     pub category_state: CategoryState,
+//     // pub product_state: ProductState,
+//     // pub order_state: OrderState,
+// }
 
-impl ModuleState {
-    pub fn new(db: PgPool, config: Config) -> Self {
-        Self {
-            admin_state: AdminState::new(db.clone()),
-            auth_state: AuthState::new(db.clone()),
-        }
-    }
-}
+// impl ModuleState {
+//     pub fn new(db: PgPool, config: Config) -> Self {
+//         Self {
+//             admin_state: AdminState::new(db.clone()),
+//             auth_state: AuthState::new(db.clone()),
+//         }
+//     }
+// }
