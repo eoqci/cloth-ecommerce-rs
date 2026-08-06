2d58f21f459691316439bdce421e482e19966d85
eoqci <boyrovip1539@gmail.com>
2026-08-06 23:05:01 +0700

    feat: user state

--- src/modules/user/state.rs
+++ src/modules/user/state.rs
@@ -2,16 +2,21 @@ use std::sync::Arc;
 
 use sqlx::PgPool;
 
-use crate::modules::user::repository::UserRepository;
+use crate::modules::user::{repository::UserRepository, service::UserService};
 
 pub struct UserState {
     pub user_repo: Arc<UserRepository>,
+    pub user_service: UserService,
 }
 
 impl UserState {
     pub fn new(db: PgPool) -> Self {
         let user_repo = Arc::new(UserRepository::new(db.clone()));
 
-        Self { user_repo }
+        let user_service = UserService::new(user_repo.clone());
+        Self {
+            user_repo,
+            user_service,
+        }
     }
 }
