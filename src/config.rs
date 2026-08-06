5abb7b2608602bf5987ef1495311ab364bda1d7e
eoqci <boyrovip1539@gmail.com>
2026-08-06 22:25:48 +0700

    feat: config update

--- src/config.rs
+++ src/config.rs
@@ -7,9 +7,11 @@ use std::{env, str::FromStr};
 pub struct Config {
     //db config env
     pub database_url: String,
-    //redis
     pub redis_url: String,
+    // prod - test
+    pub app_env: String,
     //domain - host env
+    pub fe_url: String,
     pub server_host: String,
     pub server_port: u16,
     pub domain_name: String,
@@ -20,6 +22,10 @@ pub struct Config {
     pub jwt_secret: String,
     pub jwt_expired_in: i64,
     pub refresh_token_expired_in: i32,
+    // google config
+    pub google_client_id: String,
+    pub google_client_secret: String,
+    pub google_redirect_uri: String,
     //cloudflare env
     pub cf_r2_endpoint: String,
     pub cf_r2_access_key: String,
@@ -59,20 +65,38 @@ impl Config {
         Ok(Config {
             database_url: Self::get_env("DATABASE_URL")?,
             redis_url: Self::get_env("REDIS_URL")?,
+
+            //==============================|  SERVER CONFIG & FE |==============================
             server_host: env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_string()),
             server_port: Self::get_number_env::<u16>("PORT")?,
             domain_name: Self::get_env("DOMAIN_NAME")?,
+
+            fe_url: Self::get_env("FE_URL")?,
+
+            //==============================| EMAIL |==============================
             resend_api_key: Self::get_env("RESEND_API_KEY")?,
             from_email: Self::get_email_env("FROM_EMAIL")?,
+
+            //==============================| JWT |==============================
             jwt_secret: Self::get_env("JWT_SECRET")?,
             jwt_expired_in: Self::get_number_env::<i64>("JWT_EXPIRED_IN")?,
             refresh_token_expired_in: Self::get_number_env::<i32>("REFRESH_TOKEN_EXPIRED_IN")?,
+
+            //==============================| GOOGLE |==============================
+            google_client_id: Self::get_env("GOOGLE_CLIENT_ID").expect("GOOGLE_CLIENT_ID"),
+            google_client_secret: Self::get_env("GOOGLE_CLIENT_SECRET")
+                .expect("GOOGLE_CLIENT_SECRET"),
+            google_redirect_uri: Self::get_env("GOOGLE_REDIRECT_URI").expect("GOOGLE_REDIRECT_URI"),
+
             //==============================| CLOUDFLARE |==============================
             cf_r2_access_key: Self::get_env("CF_R2_ACCESS_KEY")?,
             cf_r2_secret_key: Self::get_env("CF_R2_SECRET_KEY")?,
             cf_r2_endpoint: Self::get_env("CF_R2_ENDPOINT")?,
             cf_r2_bucket: Self::get_env("CF_R2_BUCKET")?,
             public_asset_url: Self::get_env("PUBLIC_ASSET_URL")?,
+
+            //==============================| TELEMETRY CONFIG |==============================
+            app_env: Self::get_env("APP_ENV")?,
         })
     }
 }
