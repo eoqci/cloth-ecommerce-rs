2af59090d72728a59682567a35dda816cf1dfa7a
eoqci <boyrovip1539@gmail.com>
2026-08-07 21:59:14 +0700

    feat: find user by id/email

--- src/modules/auth/repository.rs
+++ src/modules/auth/repository.rs
@@ -1,5 +1,6 @@
 use crate::modules::user::model::{AuthProvider, User, UserRole, UserStatus};
 use sqlx::{Error, PgPool};
+use uuid::Uuid;
 
 #[derive(Clone)]
 pub struct AuthRepository {
@@ -56,75 +57,59 @@ impl AuthRepository {
             provider as AuthProvider, // Passed explicitly for the macro
             provider_id
         )
-        .fetch_one(&self.pool) // Make sure to replace `&self.pool` with your actual connection pool field
+        .fetch_one(&self.pool)
         .await?;
 
         Ok(user)
     }
 
-    pub async fn find_by_email(&self, email: &str) -> Result<Option<User>, Error> {
-        let user = sqlx::query_as!(
+    pub async fn find_user_by_id(&self, id: Uuid) -> Result<Option<User>, sqlx::Error> {
+        sqlx::query_as!(
             User,
             r#"
-                SELECT
-                    id,
-                    email,
-                    password_hash,
-                    name,
-                    avatar_url,
-                    description,
-                    role as "role: UserRole",
-                    status as "status: UserStatus",
-                    provider as "provider: AuthProvider",
-                    provider_id,
-                    created_at,
-                    updated_at
-                FROM users
-                WHERE email = $1
+                    SELECT
+                        id,
+                        email,
+                        name,
+                        avatar_url,
+                        description,
+                        role AS "role: UserRole",
+                        status AS "status: UserStatus",
+                        provider AS "provider: AuthProvider",
+                        provider_id,
+                        created_at,
+                        updated_at
+                    FROM users
+                    WHERE id = $1
+                "#,
+            id
+        )
+        .fetch_optional(&self.pool)
+        .await
+    }
+
+    pub async fn find_user_by_email(&self, email: &str) -> Result<Option<User>, sqlx::Error> {
+        sqlx::query_as!(
+            User,
+            r#"
+                    SELECT
+                        id,
+                        email,
+                        name,
+                        avatar_url,
+                        description,
+                        role AS "role: UserRole",
+                        status AS "status: UserStatus",
+                        provider AS "provider: AuthProvider",
+                        provider_id,
+                        created_at,
+                        updated_at
+                    FROM users
+                    WHERE lower(email) = lower($1)
                 "#,
             email
         )
         .fetch_optional(&self.pool)
-        .await?;
-
-        Ok(user)
-    }
-
-    pub async fn active_user(&self, user_id: uuid::Uuid) -> Result<(), Error> {
-        sqlx::query!(
-            r#"
-                UPDATE users
-                SET status = 'active'::user_status_type, updated_at = NOW()
-                WHERE id = $1
-            "#,
-            user_id
-        )
-        .execute(&self.pool)
-        .await?;
-
-        Ok(())
-    }
-
-    pub async fn create_session(
-        &self,
-        user_id: uuid::Uuid,
-        refresh_token: &str,
-        user_agent: &str,
-        expires_in_day: i32,
-    ) -> Result<(), Error> {
-        sqlx::query!(
-            r#"
-                INSERT INTO user_sessions (user_id, refresh_token, user_agent, expires_at)
-                VALUES ($1, $2, $3, NOW() + make_interval(days => $4))
-            "#,
-            user_id,
-            refresh_token,
-            user_agent,
-            expires_in_day
-        )
-        .execute(&self.pool)
-        .await?;
-
-        Ok(())
+        .await
     }
 }
