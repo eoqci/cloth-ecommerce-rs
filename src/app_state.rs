9310568a1bc16fcfbe8bb554a86c99e072aed96b
eoqci <boyrovip1539@gmail.com>
2026-08-09 20:04:37 +0700

    refactor: appstate
    I will add back soon but not this time, too many thing need to be done
    first.

--- src/app_state.rs
+++ src/app_state.rs
@@ -1,21 +1,7 @@
-use deadpool_redis::Pool;
 use sqlx::PgPool;
 use std::sync::Arc;
 
-use crate::{
-    config::Config,
-    infra::{mail::ResendMailService, redis::client::RedisInfra, storage::r2::UploadService},
-    modules::{
-        admin::repository::AdminRepository,
-        auth::AuthRepository,
-        category::repository::CategoryRepository,
-        module_state::ModuleState,
-        order::repository::OrderRepository,
-        product::repository::ProductRepository,
-        user::{repository::UserRepository, service::UserService},
-    },
-    shared::{ports::mail::MailService, services::jwt::TokenService},
-};
+use crate::{config::Config, errors::AppError, modules::auth::state::AuthState};
 
 #[derive(Clone)]
 pub struct AppState {
@@ -23,78 +9,26 @@ pub struct AppState {
     pub config: Arc<Config>,
     // ======================| POOL |========================
     pub db: PgPool,
-    pub redis_pool: Pool,
-    // ==================| SERVICE STATE |===================
-    pub mail_service: Arc<dyn MailService>,
-    pub token_service: Arc<TokenService>,
-    pub upload_service: Arc<UploadService>,
-    // =============| BUSINESS SERVICE STATE |===============
-    pub user_service: Arc<UserService>,
-    // ==================| REPO STATE |======================
-    pub admin_repo: Arc<AdminRepository>,
-    pub auth_repo: Arc<AuthRepository>,
-    pub user_repo: Arc<UserRepository>,
-    pub category_repo: Arc<CategoryRepository>,
-    pub product_repo: Arc<ProductRepository>,
-    pub order_repo: Arc<OrderRepository>,
-
-    pub module_state: Arc<ModuleState>,
+    // ==================| MODULE STATE |======================
+    pub auth_state: AuthState,
 }
 
 impl AppState {
-    pub fn new(config: Config, db: PgPool) -> Self {
-        let config = Arc::new(config);
+    pub fn new(
+        config: Arc<Config>,
+        db: PgPool,
+        oauth_http_client: oauth2::reqwest::Client,
+    ) -> Result<Self, AppError> {
+        //=======================================================
+        // =====================| MODULE STATE |===================
+        //=======================================================
 
-        let email = format!("Shop Cua Vo <{}>", config.from_email);
-        let mail_service: Arc<dyn MailService> =
-            Arc::new(ResendMailService::new(config.resend_api_key.clone(), email));
-        let redis_pool = RedisInfra::init_pool(&config.redis_url);
-        let token_service: Arc<TokenService> = Arc::new(TokenService::new(
-            config.jwt_secret.clone(),
-            config.jwt_expired_in.clone(),
-        ));
-        //=======================================================
-        // =====================| REPO STATE |===================
-        //=======================================================
-        let admin_repo: Arc<AdminRepository> = Arc::new(AdminRepository::new(db.clone()));
-        let auth_repo: Arc<AuthRepository> = Arc::new(AuthRepository::new(db.clone()));
-        let user_repo: Arc<UserRepository> = Arc::new(UserRepository::new(db.clone()));
-        let category_repo: Arc<CategoryRepository> = Arc::new(CategoryRepository::new(db.clone()));
-        let product_repo: Arc<ProductRepository> = Arc::new(ProductRepository::new(db.clone()));
-        let order_repo: Arc<OrderRepository> = Arc::new(OrderRepository::new(db.clone()));
+        let auth_state = AuthState::new(db.clone(), oauth_http_client.clone(), config.clone())?;
 
-        //=======================================================
-        // ======================| SERVICE |=====================
-        //=======================================================
-        let upload_service = Arc::new(UploadService::new(
-            config.cf_r2_endpoint.clone(),
-            config.cf_r2_access_key.clone(),
-            config.cf_r2_secret_key.clone(),
-            config.cf_r2_bucket.clone(),
-            config.public_asset_url.clone(), // Cái tên miền images.domain.com á
-        ));
-        //=======================================================
-        // =================| BUSINESS SERVICE |=================
-        //=======================================================
-        let user_service: Arc<UserService> = Arc::new(UserService::new(user_repo.clone()));
-        Self {
-            // Config
+        Ok(Self {
             config,
             db,
-            // Service
-            redis_pool,
-            mail_service,
-            upload_service,
-            token_service,
-            // Business Service
-            user_service,
-            // Repo
-            admin_repo,
-            auth_repo,
-            user_repo,
-            category_repo,
-            product_repo,
-            order_repo,
-        }
+            auth_state,
+        })
     }
 }
