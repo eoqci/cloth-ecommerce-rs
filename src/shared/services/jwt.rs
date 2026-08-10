c7bde79f6eea9e8d99b44fcf9d41deb98c981fc9
eoqci <boyrovip1539@gmail.com>
2026-08-10 21:14:18 +0700

    feat: update jwt config
    - remove email from claim, replace email field with role field.
    - iat,exp change from usize to i64.
    
    - change exp: expires_in_minutes to expires_in_seconds (because of jwt
      RFC 7519 standard, unix timestamp count as seconds not minutes)

--- src/shared/services/jwt.rs
+++ src/shared/services/jwt.rs
@@ -7,43 +7,42 @@ use rand::{
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-use crate::errors::AppError;
+use crate::{errors::AppError, modules::user::model::UserRole};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Claims {
-    pub sub: String,
-    pub iat: usize,
-    pub exp: usize,
-    pub email: String,
+    pub sub: Uuid,
+    pub role: UserRole,
+    pub iat: i64,
+    pub exp: i64,
 }
 
 #[derive(Clone, Serialize)]
 pub struct TokenService {
     secret: String,
-    expires_in_minutes: i64,
+    // RFC 7519 - standard (Unix timestamp count as second)
+    expires_in_seconds: i64,
 }
 
 impl TokenService {
-    pub fn new(secret: String, expires_in_minutes: i64) -> Self {
+    pub fn new(secret: String, expires_in_seconds: i64) -> Self {
         Self {
             secret,
-            expires_in_minutes,
+            expires_in_seconds,
         }
     }
 
-    pub fn generate_access_token(&self, user_id: Uuid, email: String) -> Result<String, AppError> {
+    pub fn generate_access_token(&self, user_id: Uuid, role: UserRole) -> Result<String, AppError> {
         let now = Utc::now();
-        let expiry = now + Duration::minutes(self.expires_in_minutes);
-
         let claims = Claims {
-            sub: user_id.to_string(),
-            iat: now.timestamp() as usize,
-            exp: expiry.timestamp() as usize,
-            email,
+            sub: user_id,
+            role,
+            iat: now.timestamp(),
+            exp: (now + Duration::seconds(self.expires_in_seconds)).timestamp(),
         };
 
         encode(
-            &Header::default(),
+            &Header::new(jsonwebtoken::Algorithm::HS256),
             &claims,
             &EncodingKey::from_secret(self.secret.as_bytes()),
         )
@@ -54,18 +53,13 @@ impl TokenService {
         Alphanumeric.sample_string(&mut rand::rng(), 64)
     }
 
-    pub fn decode_access_token(&self, token: &str) -> Result<Claims, AppError> {
+    pub fn verify_access_token(&self, token: &str) -> Result<Claims, AppError> {
         decode::<Claims>(
             token,
-            &DecodingKey::from_secret(self.secret.as_bytes()),
-            &Validation::default(),
+            &DecodingKey::from_secret(&self.secret.as_bytes()),
+            &Validation::new(jsonwebtoken::Algorithm::HS256),
         )
         .map(|data| data.claims)
-        .map_err(|e| match e.kind() {
-            jsonwebtoken::errors::ErrorKind::ExpiredSignature => {
-                AppError::Unauthorized("Token expired".to_string())
-            }
-            _ => AppError::Unauthorized("Invalid Token".to_string()),
-        })
+        .map_err(|_| AppError::Unauthorized("Invalid or expired token".to_string()))
     }
 }
