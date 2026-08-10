02995621b08a0dd0e0bacc3e89e6f06b0ea534c6
eoqci <boyrovip1539@gmail.com>
2026-08-10 21:42:08 +0700

    feat: update GoogleUserInfo matching with the repository - service

--- src/modules/auth/dto.rs
+++ src/modules/auth/dto.rs
@@ -1,5 +1,7 @@
 use serde::{self, Deserialize};
 
+use crate::modules::user::model::AuthProvider;
+
 #[derive(Deserialize)]
 pub struct GoogleUserInfo {
     // "sub" - because google return as "sub"
@@ -8,6 +10,7 @@ pub struct GoogleUserInfo {
     pub google_id: String,
     pub name: String,
     pub email: String,
+    pub provider: AuthProvider,
     #[serde(rename = "picture")]
-    pub avatar: Option<String>,
+    pub avatar_url: Option<String>,
 }
