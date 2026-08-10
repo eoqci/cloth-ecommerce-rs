4afe78cec8435887c87c223ae3d9562d6a36e7b4
eoqci <boyrovip1539@gmail.com>
2026-08-10 21:09:36 +0700

    feat: add GoogleUserInfo dto for login

--- src/modules/auth/dto.rs
+++ src/modules/auth/dto.rs
@@ -1,60 +1,13 @@
-use std::borrow::Cow;
+use serde::{self, Deserialize};
 
-use serde::{Deserialize, Serialize};
-use validator::{Validate, ValidationError};
-
-#[derive(Deserialize, Serialize, Validate)]
-pub struct RegisterRequest {
-    #[validate(length(min = 1, message = "Tên không được để trống"))]
+#[derive(Deserialize)]
+pub struct GoogleUserInfo {
+    // "sub" - because google return as "sub"
+    // not google_id but we need change for readability
+    #[serde(rename = "sub")]
+    pub google_id: String,
     pub name: String,
-
-    #[validate(email(message = "Email không hợp lệ"))]
     pub email: String,
-
-    #[validate(
-        length(min = 6, max = 20, message = "Mật khẩu phải từ 6-20 ký tự"),
-        custom(function = "validate_password")
-    )]
-    pub password: String,
-}
-
-#[derive(Deserialize, Serialize, Validate)]
-pub struct LoginRequest {
-    #[validate(email(message = "Email không hợp lệ"))]
-    pub email: String,
-
-    #[validate(
-        length(min = 6, max = 20, message = "Mật khẩu phải từ 6-20 ký tự"),
-        custom(function = "validate_password")
-    )]
-    pub password: String,
-}
-
-#[derive(Deserialize, Serialize, Validate)]
-pub struct VerifyEmailRequest {
-    #[validate(email(message = "Email không hợp lệ"))]
-    pub email: String,
-
-    #[validate(length(min = 6, message = "Token không hợp lệ"))]
-    pub token: String,
-}
-
-#[derive(Deserialize, Serialize, Validate)]
-pub struct ResendVerifyEmailRequest {
-    #[validate(email(message = "Email không hợp lệ"))]
-    pub email: String,
-}
-
-//Helper func
-fn validate_password(password: &str) -> Result<(), ValidationError> {
-    let has_letter = password.chars().any(|c| c.is_ascii_alphabetic());
-    let has_digit = password.chars().any(|c| c.is_ascii_digit());
-
-    if !has_letter || !has_digit {
-        let mut error = ValidationError::new("password_complexity");
-        error.message = Some(Cow::from("Mật khẩu phải chứa cả chữ cái và số"));
-        return Err(error);
-    }
-
-    Ok(())
+    #[serde(rename = "picture")]
+    pub avatar: Option<String>,
 }
