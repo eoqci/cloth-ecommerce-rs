a160dd4bca3c57684c901565fe4ca298b30734ea
eoqci <boyrovip1539@gmail.com>
2026-08-10 21:43:39 +0700

    feat: google_login [DONE]

--- src/modules/auth/service.rs
+++ src/modules/auth/service.rs
@@ -1,7 +1,9 @@
 use std::sync::Arc;
 
 use crate::{
-    config::Config, errors::AppError, modules::auth::AuthRepository,
+    config::Config,
+    errors::AppError,
+    modules::auth::{AuthRepository, dto::GoogleUserInfo, error::AuthError},
     shared::services::jwt::TokenService,
 };
 
@@ -37,5 +39,41 @@ impl AuthService {
         gg_user: GoogleUserInfo,
         user_agent: Option<&str>,
     ) -> Result<TokenPair, AppError> {
+        let user = self
+            .auth_repo
+            .find_or_create_by_google(
+                &gg_user.email,
+                &gg_user.name,
+                gg_user.avatar_url.as_deref(),
+                gg_user.provider,
+                &gg_user.google_id,
+            )
+            .await
+            .map_err(|e| {
+                tracing::error!("Database Error finding/creating user: {:?}", e);
+                AuthError::AuthorizationFailed
+            })?;
+
+        let access_token = self
+            .token_sevice
+            .generate_access_token(user.id, user.role)?;
+
+        let refresh_token = self.token_sevice.generate_refresh_token();
+
+        self.auth_repo
+            .create_session(
+                user.id,
+                &refresh_token,
+                user_agent,
+                self.config.refresh_token_expired_in,
+                None,
+            )
+            .await
+            .map_err(|_| AuthError::AuthorizationFailed)?;
+
+        Ok(TokenPair {
+            access_token,
+            refresh_token,
+        })
     }
 }
