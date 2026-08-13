5f83e4ab3ae614393eeccacdb9a6836d9a9b4359
eoqci <boyrovip1539@gmail.com>
2026-08-13 21:26:38 +0700

    feat: login feature with google_callback, refresh_token, logout

--- src/modules/auth/service.rs
+++ src/modules/auth/service.rs
@@ -1,9 +1,17 @@
 use std::sync::Arc;
 
+use oauth2::{AuthorizationCode, PkceCodeVerifier, TokenResponse};
+
 use crate::{
     config::Config,
     errors::AppError,
-    modules::auth::{AuthRepository, dto::GoogleUserInfo, error::AuthError},
+    modules::{
+        auth::{
+            AuthRepository, RotateOutcome, dto::GoogleUserInfo, error::AuthError,
+            oauth::GoogleOAuthClient,
+        },
+        user::model::{AuthProvider, User},
+    },
     shared::services::jwt::TokenService,
 };
 
@@ -12,6 +20,7 @@ pub struct AuthService {
     auth_repo: Arc<AuthRepository>,
     token_sevice: Arc<TokenService>,
     config: Arc<Config>,
+    oauth_client: GoogleOAuthClient,
     http_client: oauth2::reqwest::Client,
 }
 
@@ -19,19 +28,95 @@ pub struct TokenPair {
     pub access_token: String,
     pub refresh_token: String,
 }
+
+pub struct AuthResult {
+    pub user: User,
+    pub access_token: String,
+    pub raw_refresh_token: String,
+}
 impl AuthService {
     pub fn new(
         auth_repo: Arc<AuthRepository>,
         token_sevice: Arc<TokenService>,
         config: Arc<Config>,
+        oauth_client: GoogleOAuthClient,
         http_client: oauth2::reqwest::Client,
     ) -> Self {
         Self {
             auth_repo,
             token_sevice,
             config,
+            oauth_client,
             http_client,
         }
+    }
+
+    pub async fn handle_google_callback(
+        &self,
+        code: String,
+        pkce_verifier: PkceCodeVerifier,
+        user_agent: Option<&str>,
+    ) -> Result<AuthResult, AppError> {
+        let token_result = self
+            .oauth_client
+            .exchange_code(AuthorizationCode::new(code))
+            .set_pkce_verifier(pkce_verifier)
+            .request_async(&self.http_client)
+            .await
+            .map_err(|e| AppError::ExternalService {
+                service: "google_oauth_token".into(),
+                source: anyhow::anyhow!(e.into()),
+            })?;
+
+        let userinfo: GoogleUserInfo = self
+            .http_client
+            .get("https://www.googleapis.com/oauth2/v3/userinfo")
+            .bearer_auth(token_result.access_token().secret())
+            .send()
+            .await
+            .map_err(|e| AppError::ExternalService {
+                service: "google_userinfo".to_string(),
+                source: anyhow::anyhow!(e.to_string()),
+            })?
+            .json()
+            .await
+            .map_err(|e| AppError::ExternalService {
+                service: "google_userinfo".to_string(),
+                source: anyhow::anyhow!(e.to_string()),
+            })?;
+
+        let user = self
+            .auth_repo
+            .find_or_create_by_google(
+                &userinfo.email,
+                &userinfo.name,
+                userinfo.avatar_url.as_deref(),
+                AuthProvider::Google,
+                &userinfo.sub,
+            )
+            .await?;
+        let raw_refresh_token = self.token_sevice.generate_refresh_token();
+        let hash = &self.token_sevice.hash_refresh_token(&raw_refresh_token);
+
+        self.auth_repo
+            .create_session(
+                user.id,
+                &hash,
+                user_agent,
+                self.config.refresh_token_expired_in,
+                None,
+            )
+            .await?;
+
+        let access_token = self
+            .token_sevice
+            .generate_access_token(user.id, user.role)?;
+
+        Ok(AuthResult {
+            user,
+            access_token,
+            raw_refresh_token,
+        })
     }
 
     pub async fn google_login(
@@ -78,16 +163,69 @@ impl AuthService {
         })
     }
 
-    pub async fn refresh_access_token(
+    pub async fn refresh_token(
         &self,
-        old_refresh_token: &str,
+        raw_refresh_token: &str,
+        user_agent: Option<&str>,
     ) -> Result<TokenPair, AppError> {
-        let new_refresh_token = self.token_sevice.generate_refresh_token();
+        let hash = self.token_sevice.hash_refresh_token(raw_refresh_token);
 
-        let result = self.auth_repo.
+        let outcome: RotateOutcome = match self.auth_repo.mark_session_used_if_valid(&hash).await? {
+            Some(o) => o,
+            None => {
+                // cases: none exists/expired (normal) or reuse (be careful :v)
+                if let Some(session) = self.auth_repo.find_session_by_token_hash(&hash).await? {
+                    if session.is_used {
+                        //reused - revoke whole family, not return the reason to client
+                        let _ = self
+                            .auth_repo
+                            .revoke_session_family(session.session_family_id)
+                            .await;
+                    }
+                }
+                return Err(AppError::Unauthorized(
+                    "Invalid or expired refresh token".to_string(),
+                ));
+            }
+        };
+
+        let user = self
+            .auth_repo
+            .find_user_by_id(outcome.id)
+            .await?
+            .ok_or_else(|| AppError::Unauthorized("User not found".to_string()))?;
+
+        let raw_new_refresh_token = self.token_sevice.generate_refresh_token();
+        let new_hash = self.token_sevice.hash_refresh_token(&raw_new_refresh_token);
+
+        self.auth_repo
+            .create_session(
+                outcome.user_id,
+                &new_hash,
+                user_agent,
+                self.config.refresh_token_expired_in,
+                Some(outcome.session_family_id),
+            )
+            .await?;
+
+        let access_token = self
+            .token_sevice
+            .generate_access_token(outcome.user_id, user.role)?;
+
         Ok(TokenPair {
-            access_token: String::new(),
-            refresh_token: String::new(),
+            access_token,
+            refresh_token: raw_new_refresh_token,
         })
+    }
+
+    // logout - revoke only the one (session) who out
+    pub async fn logout(&self, raw_refresh_token: &str) -> Result<(), AppError> {
+        let hash = self.token_sevice.hash_refresh_token(raw_refresh_token);
+
+        if let Some(session) = self.auth_repo.find_session_by_token_hash(&hash).await? {
+            self.auth_repo.revoke_session_family(session.id).await?;
+        }
+
+        Ok(())
     }
 }
