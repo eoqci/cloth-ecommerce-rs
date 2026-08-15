a7d4b516f6f551dab56043643ad5ec2f1d4ec33b
eoqci <boyrovip1539@gmail.com>
2026-08-15 23:40:20 +0700

    feat: cookie

---
+++ src/modules/auth/cookies.rs
@@ -0,0 +1,8 @@
+pub const ACCESS_TOKEN_COOKIE: &str = "access_token";
+pub const REFRESH_TOKEN: &str = "refresh_token";
+pub const CSRF_STATE_COOKIE: &str = "oauth_csrf_state";
+pub const PKCE_VERIFIER_COOKIE: &str = "oauth_pkce_verifier";
+
+pub const ACESS_TOKEN_PATH: &str = "/";
+
+pub const AUTH_PATH: &str = "api/v1/auth";
