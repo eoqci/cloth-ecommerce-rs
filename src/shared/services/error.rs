0d82bec970d4ba499c8cded9aa1f3ed79bb27e88
eoqci <boyrovip1539@gmail.com>
2026-08-17 22:31:14 +0700

    delete: delete some unused features

--- src/shared/services/error.rs
+++
@@ -1,9 +0,0 @@
-use thiserror::Error;
-
-#[derive(Debug, Error)]
-pub enum SecurityError {
-    #[error("Handling failed")]
-    HashError,
-    #[error("Invalid credential")]
-    InvalidCredential,
-}
