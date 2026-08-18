94869011f0e05d946b87f070c262b2e1a4905ddf
eoqci <boyrovip1539@gmail.com>
2026-08-18 21:21:42 +0700

    feat: repo refactor [WORKING]

--- src/modules/category/repository.rs
+++ src/modules/category/repository.rs
@@ -1,12 +1,5 @@
 use sqlx::PgPool;
-use uuid::Uuid;
 
-use crate::{
-    error::AppError,
-    modules::{cart::dto::CartItemDb, category::model::Category},
-};
-
-#[derive(Clone)]
 pub struct CategoryRepository {
     pool: PgPool,
 }
@@ -14,47 +7,5 @@ pub struct CategoryRepository {
 impl CategoryRepository {
     pub fn new(pool: PgPool) -> Self {
         Self { pool }
-    }
-
-    pub async fn create(
-        &self,
-        name: &str,
-        slug: &str,
-        parent_id: Option<i32>,
-    ) -> Result<Category, AppError> {
-        let category = sqlx::query_as::<_, Category>(
-            r#"
-                INSERT INTO categories (name, slug, parent_id)
-                VALUES ($1,$2, $3)
-                RETURNING *
-            "#,
-        )
-        .bind(name)
-        .bind(slug)
-        .bind(parent_id)
-        .fetch_one(&self.pool)
-        .await
-        .map_err(|e| {
-            tracing::error!("Lỗi tạo doanh mục: {:?}", e);
-            AppError::Database(e)
-        })?;
-
-        Ok(category)
-    }
-
-    pub async fn get_all(&self) -> Result<Vec<Category>, AppError> {
-        // Should take a look: ORDER BY parent_id NULLS FIRST
-        // This helps root categories (those without a parent) always stay at the top, making tree structure easier.
-        let categories = sqlx::query_as::<_, Category>(
-            "SELECT * FROM categories ORDER BY parent_id ASC NULLS FIRST, id ASC",
-        )
-        .fetch_all(&self.pool)
-        .await
-        .map_err(|e| {
-            tracing::error!("Lỗi lấy danh sách danh mục: {:?}", e);
-            AppError::Database(e)
-        })?;
-
-        Ok(categories)
     }
 }
--- src/modules/mod.rs
+++ src/modules/mod.rs
@@ -1,7 +1,7 @@
 // pub mod admin;
 pub mod auth;
 // pub mod cart;
-// pub mod category;
+pub mod category;
 // pub mod module_state;
 // pub mod order;
 // pub mod product;
